45ce0f14aafaff0e6ec216355c9784c564d47dba
hitkuangye <85359381+hitkuangye@users.noreply.github.com>
2021-09-22 21:59:14 +0800

    Add files via upload

---
+++ shims.d.ts
@@ -0,0 +1,12 @@
+// Auto-generated. Do not edit.
+
+
+
+    //% color=50 weight=80
+    //% icon="\uf1eb"
+declare namespace i2c {
+}
+
+declare namespace Bit_IR {
+}
+// Auto-generated. Do not edit. Really.
